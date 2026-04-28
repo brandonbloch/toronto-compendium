c01594619a4b5d28661912eb7a867a9de4de1843
Brandon Bloch <brandon@bloch.ca>
2026-04-28 18:52:24 -0400

    Start expanding structure for additional entry data

--- src/data/types.ts
+++ src/data/types.ts
@@ -6,8 +6,17 @@ export enum CompendiumCategory {
   Stations = 'Stations',
 }
 
+export interface CompendiumLocation {
+  name?: string;
+  address?: string;
+  coords?: [number, number];
+}
+
 export interface CompendiumEntry {
   id: string;
   name: string;
   category: CompendiumCategory;
+  location?: CompendiumLocation | string;
+  date?: string;
+  description?: string;
 }
